97ed126c469c04bc66f1cec5f07170cc3465bbe4
macuzi <lamberto.asghedom@sentry.io>
2026-05-22 13:39:27 -0700

    minimal Clack color prompts with first validation helper.

--- index.ts
+++ index.ts
@@ -8,7 +8,6 @@ async function main() {
   const colors = await group({
     foreground: () => text({
       message: 'What is your foreground color?',
-      // make sure 3 or 6
       // trim and to lowercase
       validate: (value) => {
         return checkColorInput(value)
@@ -18,28 +17,32 @@ async function main() {
     background: ({ results }) => text({
       message: 'What is your background color?',
       validate: (value) => {
-        if (!value) {
-          return 
+        if (value !== undefined && results.foreground) {
+          return checkColorInput(value)
         }
       }
     })
   })
 
-  console.log(colors)
+  // for each color map and pass color to normalize
+  for (const color of Object.values(colors)) {
+    normalizeHexColor(color)
+  }
 }
 
-function checkColorInput(value: any) {
+function checkColorInput(value: string) {
    if (!value || value.length !== 3 && value.length !== 6) {
     return 'please enter a valid value'
   }
 }
 
-function normalizeHexColor(value: object): string {
+function normalizeHexColor(value: string): string {
   const hash = '#'
   
   const colorCode = regex.test(value) ? hash.concat(value).trim() : ''
 
-  return colorCode
+   console.log(colorCode)
+   return colorCode
 }
 
 main()
