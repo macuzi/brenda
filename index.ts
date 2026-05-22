7fa84fb504a1a03d2c9753582b5c2ef1dfdba05d
macuzi <lamberto.asghedom@sentry.io>
2026-05-22 09:31:35 -0700

    checking str.length for 3 or 5

--- index.ts
+++ index.ts
@@ -1,19 +1,26 @@
 import { text, group, select, confirm, isCancel } from "@clack/prompts";
 
+
+const regex = /^[a-zA-Z0-9]*$/
+
 async function main() {
   // asks user for foreground/background colors
   const colors = await group({
     foreground: () => text({
       message: 'What is your foreground color?',
+      // make sure 3 or 5 
+      // trim and to lowercase
       validate: (value) => {
-        if (!value || value.length !== 3) {
-          return 'you must include a foreground color'
+        if (!value) return 'Please enter a valid hex color'
+
+        if (value.length !== 3 && value.length !== 5) {
+          return 'Please include a valid foreground color'
         }
       }
     }),
 
     background: ({ results }) => text({
-      message: 'What is yoru background color?',
+      message: 'What is your background color?',
       placeholder: 'enter',
       validate: (value) => {
         if (!value) {
