c64bba5a10542ad3d1e2c8d1c5961ea23ecf6c57
macuzi <lamberto.asghedom@sentry.io>
2026-05-23 17:04:18 -0700

    chore: add prettier formatting

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,30 +1,32 @@
-import { group, text, cancel } from "@clack/prompts";
-import { checkColorInput, normalizeHexColor } from "../contrast";
+import { group, text, cancel } from '@clack/prompts';
+import { checkColorInput, normalizeHexColor } from '../contrast';
 
 async function main() {
   // asks user for foreground/background colors
-  const colors = await group({
-    foreground: () =>
-      text({
-        message: "What is your foreground color?",
-        validate: (value) => {
-          return checkColorInput(value);
-        },
-      }),
-    background: () =>
-      text({
-        message: "What is your background color?",
-        validate: (value) => {
-          return checkColorInput(value);
-        },
-      }),
-  },
-  {
-    onCancel: ({ results }) => {
-      cancel("Operation cancelled.");
-      process.exit(0);
+  const colors = await group(
+    {
+      foreground: () =>
+        text({
+          message: 'What is your foreground color?',
+          validate: (value) => {
+            return checkColorInput(value);
+          },
+        }),
+      background: () =>
+        text({
+          message: 'What is your background color?',
+          validate: (value) => {
+            return checkColorInput(value);
+          },
+        }),
     },
-  });
+    {
+      onCancel: ({ results }) => {
+        cancel('Operation cancelled.');
+        process.exit(0);
+      },
+    },
+  );
 
   // for each color map and pass color to normalize
   for (const color of Object.values(colors)) {
